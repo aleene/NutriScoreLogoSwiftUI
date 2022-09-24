988e0ce46b43e54e7a32d6dafadea634e00289fa
Arnaud Leene <aleene@gmail.com>
2022-09-24 11:07:50 +0200

    Initial Commit

---
+++ NutriScoreLogo2/NutriScoreLogo2App.swift
@@ -0,0 +1,17 @@
+//
+//  NutriScoreLogo2App.swift
+//  NutriScoreLogo2
+//
+//  Created by Arnaud Leene on 24/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct NutriScoreLogo2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
